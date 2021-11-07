104bfcbcb3eb452d30f746499bcca5a1c46feb0d
Beatriz Milz <beatriz.milz@hotmail.com>
2021-11-07 12:34:06 -0300

    muda de minusculo pra maiusculo - sys.getenv

--- inst/render_readme.R
+++ inst/render_readme.R
@@ -39,11 +39,11 @@ change_yaml_matter <- function(input_file, ..., output_file) {
 change_yaml_matter(
   "README.Rmd",
   params = list(
-    main_repo = Sys.getenv("main_repo"),
-    turma = Sys.getenv("turma"),
-    nome_curso = Sys.getenv("nome_curso"),
+    main_repo = Sys.getenv("MAIN_REPO"),
+    turma = Sys.getenv("TURMA"),
+    nome_curso = Sys.getenv("NOME_CURSO"),
     num_aula = "0",
-    download_material = Sys.getenv("download_material")
+    download_material = Sys.getenv("DOWNLOAD_MATERIAL")
   )
 ) %>%
   writeLines("README.Rmd")
