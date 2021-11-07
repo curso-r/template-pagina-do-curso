93bef268ccc5dd0828db808e4689877055d985ed
Beatriz Milz <beatriz.milz@hotmail.com>
2021-11-07 12:24:20 -0300

    altera o render_readme - usar variaveis de ambiente

--- inst/render_readme.R
+++ inst/render_readme.R
@@ -15,17 +15,18 @@ change_yaml_matter <- function(input_file, ..., output_file) {
     }
   }
   delimiters <- delimiters[1:2]
-  yaml_list <- yaml::yaml.load(input_lines[ (delimiters[1]+1):(delimiters[2]-1) ])
-  
+  yaml_list <-
+    yaml::yaml.load(input_lines[(delimiters[1] + 1):(delimiters[2] - 1)])
+
   dots <- list(...)
-  yaml_list <- c(yaml_list[ setdiff(names(yaml_list), names(dots)) ], dots)
-  
-  output_lines <- c(
-    if (delimiters[1] > 0) input_lines[1:(delimiters[1])],
+  yaml_list <-
+    c(yaml_list[setdiff(names(yaml_list), names(dots))], dots)
+
+  output_lines <- c(if (delimiters[1] > 0)
+    input_lines[1:(delimiters[1])],
     strsplit(yaml::as.yaml(yaml_list), "\n")[[1]],
-    input_lines[ -(1:(delimiters[2]-1)) ]
-  )
-  
+    input_lines[-(1:(delimiters[2] - 1))])
+
   if (missing(output_file)) {
     return(output_lines)
   } else {
@@ -35,21 +36,25 @@ change_yaml_matter <- function(input_file, ..., output_file) {
 }
 
 # mudar o yaml do arquivo .Rmd
-change_yaml_matter("README.Rmd", 
-                  params = list(main_repo = "main-web-scraping",
-                                      turma = "202111-web-scraping",
-                                      nome_curso = "Web Scraping",
-                                      num_aula = "0",
-                                      download_material = TRUE)) %>% 
+change_yaml_matter(
+  "README.Rmd",
+  params = list(
+    main_repo = Sys.getenv("main_repo"),
+    turma = Sys.getenv("turma"),
+    nome_curso = Sys.getenv("nome_curso"),
+    num_aula = "0",
+    download_material = Sys.getenv("download_material")
+  )
+) %>%
   writeLines("README.Rmd")
 
-      
+
 # render o arquivo
 
 rmarkdown::render(
-  input = "README.Rmd", 
+  input = "README.Rmd",
   output_format = "github_document",
   clean = TRUE,
-  output_file="README.md"
-    
+  output_file = "README.md"
+
 )
